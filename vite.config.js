bd0763a8e13a83068bd52f8f71ce16db58a613f7
JacksonTuttle <jacksontuttlemail@gmail.com>
2025-11-15 21:42:15 -0600

    Works when I do npm run dev

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Tuttle-sGames/',   // ← MUST MATCH REPO NAME EXACTLY
+  // base: '/Tuttle-sGames/',   // ← MUST MATCH REPO NAME EXACTLY
   plugins: [react()],
 })
