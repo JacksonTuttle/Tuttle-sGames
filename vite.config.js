120f1260fe82c88e16a22578dd06105026788024
JacksonTuttle <jacksontuttlemail@gmail.com>
2025-11-15 21:37:52 -0600

    Git pages 1.1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/TuttlesGames/',   // 👈 IMPORTANT: your repo name EXACTLY
   plugins: [react()],
 })
